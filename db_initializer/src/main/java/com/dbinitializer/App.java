4b00127bc0269407276625905d8794ce989084ee
pootaro.suzuki <pootaro.suzuki@gmail.com>
2023-01-24 02:24:10 +0900

    initial commit

---
+++ db_initializer/src/main/java/com/dbinitializer/App.java
@@ -0,0 +1,7 @@
+package com.dbinitializer;
+
+public class App {
+  public static void main(String args[]) {
+    System.out.println("started");
+  }
+}
---
+++ db_initializer/start_app.sh
@@ -0,0 +1,1 @@
+java -jar build/libs/db_initializer.jar
